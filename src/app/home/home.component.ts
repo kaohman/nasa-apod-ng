3610caf7cb33b8ed1233b526d6e869839cb447f7
Karin Ohman <kaohma@gmail.com>
2019-11-14 14:32:21 -0700

    Add dynamic dates to photos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,11 +15,25 @@ export class HomeComponent implements OnInit {
   dates: string[] = [ '2019-11-14', '2019-11-13', '2019-11-12'];
   
   ngOnInit() {
-    this.getPhotos();
+    this.getPhotosForMonth();
+  }
+
+  getDates() {
+    const currentYear = new Date().getFullYear().toString().padStart(4, '0');
+    const currentMonth = (new Date().getMonth() + 1).toString().padStart(2, '0');
+    const currentDayNum = new Date().getDate();
+
+    const dates: string[] = [];
+    for (let i: number = currentDayNum; i >= 1; i--) {
+      const currentDay = i.toString().padStart(2, '0');
+      dates.push(`${currentYear}-${currentMonth}-${currentDay}`)
+    }
+    return dates
   }
   
-  getPhotos(): void {
-      this.photosService.getPhotos(this.dates)
+  getPhotosForMonth(): void {
+      const dates: string[] = this.getDates();
+      this.photosService.getPhotos(dates)
       .subscribe(photos => this.photos =  photos);
   }
 
