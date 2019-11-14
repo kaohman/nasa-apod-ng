5eabc3744dd26b800df201742fc0e132e9bee8db
Karin Ohman <kaohma@gmail.com>
2019-11-14 16:38:16 -0700

    Add photo details page

---
+++ src/app/photo-details/photo-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PhotosService } from '../photos.service';
+import { Photo } from '../Photo';
+
+@Component({
+  selector: 'app-photo-details',
+  templateUrl: './photo-details.component.html',
+  styleUrls: ['./photo-details.component.scss']
+})
+export class PhotoDetailsComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private photosService: PhotosService
+  ) { }
+  
+  photo: Photo;
+  isLoading: boolean = true;
+
+  ngOnInit() {
+    const photoDate = this.route.snapshot.paramMap.get('id');
+    this.photosService.getPhoto(photoDate)
+      .subscribe(photo => this.photo = photo);
+    this.isLoading = false;
+  }
+
+}
